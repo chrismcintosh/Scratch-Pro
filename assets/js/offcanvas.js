df6570b50027d9ac99ad3570cc9de0112c201f23
Chris Mcintosh <chris@mcintosh.io>
2017-11-08 12:41:58 -0500

    Implement post feed and event feed

--- assets/js/offcanvas.js
+++ assets/js/offcanvas.js
@@ -60,3 +60,4 @@ export const offCanvasFunction = (offCanvasContent) => {
           
 }
 
+
--- lib/acf/acf-flexible.php
+++ lib/acf/acf-flexible.php
@@ -20,6 +20,66 @@ function scratch_pro_display_flexible_content() {
 						include get_stylesheet_directory() . '/lib/acf/flexible-components/testimonial.php';
 				} elseif( get_row_layout() == 'services' ) {
 						include get_stylesheet_directory() . '/lib/acf/flexible-components/services.php';
+				} elseif( get_row_layout() == 'events_list' ) {
+					include get_stylesheet_directory() . '/lib/acf/flexible-components/event-list.php';
+				} elseif( get_row_layout() == 'blog_posts' ) {
+					include get_stylesheet_directory() . '/lib/acf/flexible-components/blog-posts.php';
 				}
 		endwhile;
+}
+
+// Load event categories into select feild
+add_filter('acf/load_field/key=field_5a032f89c7a99', 'scratch_category_acf_select');
+function scratch_category_acf_select($field)
+{
+    $event_categories = get_terms(
+        array(
+            'taxonomy' => Tribe__Events__Main::TAXONOMY,
+            'hide_empty' => 1,
+        )
+    );
+
+    $field['choices'][''] = 'All Events';
+    foreach ($event_categories as $event_category) {
+        // Grab parent category and check if it exists
+        $event_category_parent = get_term($event_category->parent, Tribe__Events__Main::TAXONOMY);
+        if (empty($event_category_parent->name)) {
+            $event_category_name = $event_category->name;
+        } else {
+            $event_category_name = $event_category_parent->name.'/'.$event_category->name;
+        }
+        // Pass slug and name to ACF choices
+        $field['choices'][$event_category->slug] = $event_category_name;
+    }
+
+    return $field;
+}
+
+// Load event categories into select feild
+add_filter('acf/load_field/key=field_5a033cbdad7b5', 'blog_post_category_acf_select');
+function blog_post_category_acf_select($field)
+{
+    $categories = get_terms(
+        array(
+            'taxonomy' => 'category',
+            'hide_empty' => 1,
+        )
+    );
+
+    $field['choices'][''] = 'All Posts';
+    foreach ($categories as $category) {
+        // Grab parent category and check if it exists
+        $category_parent = get_term($category->parent, 'category');
+        if (empty($category_parent->name)) {
+            $category_slug = $category->slug;
+            $category_name = $category->name;
+        } else {
+            $category_slug = $category_parent->slug.'/'.$category->slug;
+            $category_name = $category_parent->name.'/'.$category->name;
+        }
+        // Pass slug and name to ACF choices
+        $field['choices'][$category_slug] = $category_name;
+    }
+
+    return $field;
 }
