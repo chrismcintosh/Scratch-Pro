45ad4a250a1bc6604b270dbeab1a413b12588039
Joey Z <joey.r.ziolkowski@gmail.com>
2017-11-20 13:03:42 -0500

    lib: Disable dashicons for now to increase page speed

--- lib/enqueue-scripts.php
+++ lib/enqueue-scripts.php
@@ -2,12 +2,12 @@
 //* Enqueue Scripts and Styles
 add_action( 'wp_enqueue_scripts', 'scratch_pro_enqueue_scripts_styles' );
 function scratch_pro_enqueue_scripts_styles() {
-    // wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION, true );
-	wp_enqueue_style(  'font-awesome',   '//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css' );
-    
-	wp_enqueue_script( 'scratch-bundle', get_stylesheet_directory_uri() . '/bundle.js');
+		wp_enqueue_style(  'font-awesome',   '//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css' );
+		wp_enqueue_script( 'scratch-bundle', get_stylesheet_directory_uri() . '/bundle.js');
 
-	wp_enqueue_style( 'dashicons' );
+		// Disabled styles
+		// wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION, true );
+		// wp_enqueue_style( 'dashicons' );
 }
 
 // Custom scripting to move JavaScript from the head to the footer
