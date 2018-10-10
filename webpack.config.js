3bef562a71f80912694e8e63c7340148b71b7bc3
Joey Z <joey.r.ziolkowski@gmail.com>
2018-10-09 21:13:51 -0400

    npm: Update packages to the latest version

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
 module.exports = {
-     entry: './assets/js/app.js',
-     output: {
-         path: '/',
-         filename: 'bundle.js'
-     },
-     externals: {
-       jquery: 'jQuery'
-     },
-     module: {
-       loaders: [
-       ],
-       rules: [
-         // JS LOADER
-         // Reference: https://github.com/babel/babel-loader
-         // Transpile .js files using babel-loader
-         // Compiles ES6 and ES7 into ES5 code
-         {
-           test: /\.js$/,
-           use: [
-             {
-               loader: 'babel-loader',
-             }
-           ],
-         }
-       ]
-     }
-   };
+    entry: {
+        app: "./assets/js/app.js"
+    },
+    output: {
+        path: "/",
+        filename: "bundle.js"
+    },
+    externals: {
+        jquery: "jQuery"
+    },
+    module: {
+        rules: [
+            // JS LOADER
+            // Reference: https://github.com/babel/babel-loader
+            // Transpile .js files using babel-loader
+            // Compiles ES6 and ES7 into ES5 code
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ]
+            }
+        ]
+    }
+};
