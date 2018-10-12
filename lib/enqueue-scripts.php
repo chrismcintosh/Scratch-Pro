6d7bf6ab074928604ac0dc00538cc4737c5d4d68
Joey Z <joey.r.ziolkowski@gmail.com>
2018-10-12 15:19:29 -0400

    lib: Enqueue FontAwesome v5.4.1

--- lib/enqueue-scripts.php
+++ lib/enqueue-scripts.php
@@ -2,12 +2,8 @@
 //* Enqueue Scripts and Styles
 add_action( 'wp_enqueue_scripts', 'scratch_pro_enqueue_scripts_styles' );
 function scratch_pro_enqueue_scripts_styles() {
-		wp_enqueue_style(  'font-awesome',   '//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css' );
-		wp_enqueue_script( 'scratch-bundle', get_stylesheet_directory_uri() . '/bundle.js');
-
-		// Disabled styles
-		// wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION, true );
-		// wp_enqueue_style( 'dashicons' );
+		wp_enqueue_style('font-awesome', '//use.fontawesome.com/releases/v5.4.1/css/all.css');
+		wp_enqueue_script('scratch-bundle', get_stylesheet_directory_uri() . '/bundle.js');
 }
 
 // Custom scripting to move JavaScript from the head to the footer
